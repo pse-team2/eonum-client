b19ba792b7da145473f09c516652f595bc6d883b
Renato Corti <renato.corti@students.unibe.ch>
2012-03-14 13:36:24 +0100

    Added map to display location
    	modified:   AndroidManifest.xml
    	modified:   project.properties
    	modified:   res/layout/location.xml
    	modified:   res/values/strings.xml
    	new file:   src/main/java/ch/eonum/MapItemizedOverlay.java
    	modified:   src/main/java/ch/eonum/ShowLocation.java

---
+++ src/main/java/ch/eonum/MapItemizedOverlay.java
@@ -0,0 +1,51 @@
+package ch.eonum;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class MapItemizedOverlay extends ItemizedOverlay
+{
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	Context mContext;
+
+	public MapItemizedOverlay(Drawable defaultMarker, Context context)
+	{
+		super(boundCenterBottom(defaultMarker));
+		this.mContext = context;
+	}
+
+	public void addOverlay(OverlayItem overlay)
+	{
+		this.mOverlays.add(overlay);
+		populate();
+	}
+
+	@Override
+	protected OverlayItem createItem(int i)
+	{
+		return this.mOverlays.get(i);
+	}
+
+	@Override
+	public int size()
+	{
+		return this.mOverlays.size();
+	}
+
+	@Override
+	protected boolean onTap(int index)
+	{
+		OverlayItem item = this.mOverlays.get(index);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(this.mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
+	}
+
+}
--- src/main/java/ch/eonum/ShowLocation.java
+++ src/main/java/ch/eonum/ShowLocation.java
@@ -1,25 +1,71 @@
 package ch.eonum;
 
 import java.util.List;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-import android.app.Activity;
+import android.widget.Toast;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 
-public class ShowLocation extends Activity implements LocationListener {
+public class ShowLocation extends MapActivity
+{
 
 	private double latitude;
 	private double longitude;
 	private LocationManager locMgr = null;
+	/* Implement Location Listener */
+	private LocationListener locLst = new LocationListener()
+	{
+		@Override
+		public void onLocationChanged(Location location)
+		{
+			ShowLocation.this.latitude = location.getLatitude();
+			ShowLocation.this.longitude = location.getLongitude();
+			String Text = "Location: " + ShowLocation.this.latitude + " : " + ShowLocation.this.longitude;
+			ShowLocation.this.locationTxt.setText(Text);
+			Log.i("Location change", "" + ShowLocation.this.latitude + " : " + ShowLocation.this.longitude);
+			Toast.makeText(getApplicationContext(), Text, Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onProviderDisabled(String provider)
+		{
+			Toast.makeText(getApplicationContext(), "Gps Disabled", Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onProviderEnabled(String provider)
+		{
+			Toast.makeText(getApplicationContext(), "Gps Enabled", Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras)
+		{
+		}
+	};
+	/* End of implemented LocationListener */
 	TextView locationTxt, list;
 	HTTPRequest request;
 
 	String resultString;
 	List<String> resultList;
+
+	MapView mapView;
+	List<Overlay> mapOverlays;
+	Drawable drawable;
+	MapItemizedOverlay itemizedOverlay;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -27,29 +73,39 @@ public class ShowLocation extends Activity implements LocationListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.location);
 
-		locationTxt = (TextView) findViewById(R.id.locationlabel);
-		locMgr = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+		this.mapView = (MapView) findViewById(R.id.mapview);
+		this.mapView.setBuiltInZoomControls(true);
+		this.mapView.setSatellite(true);
+
+		this.mapOverlays = this.mapView.getOverlays();
+		this.drawable = this.getResources().getDrawable(R.drawable.icon);
+		this.itemizedOverlay = new MapItemizedOverlay(this.drawable, this);
+
+		// Use the LocationManager class to obtain GPS locations
+		this.locationTxt = (TextView) findViewById(R.id.locationlabel);
+		this.locMgr = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		// Register the listener with the Location Manager to receive location updates
+		this.locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this.locLst);
 	}
 
 	@Override
-	public void onLocationChanged(Location location) {
-		this.latitude = location.getLatitude();
-		this.longitude = location.getLongitude();
-		this.locationTxt.setText("Location: " + this.latitude + " : " + this.longitude);
-		Log.i("Location change", "" + latitude + " : " + longitude);
-		
+	protected boolean isRouteDisplayed()
+	{
+		return false;
 	}
 
-	@Override
-	public void onProviderDisabled(String provider) {
+	public void onPostExecute()
+	{
+		GeoPoint initGeoPoint = new GeoPoint(
+			(int) (this.locMgr.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude() * 1000000),
+			(int) (this.locMgr.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude() * 1000000)
+			);
+		OverlayItem overlayitem = new OverlayItem(initGeoPoint, "", "");
+
+		this.itemizedOverlay.addOverlay(overlayitem);
+		this.mapOverlays.add(this.itemizedOverlay);
+
+		this.locMgr.removeUpdates(this.locLst);
 	}
 
-	@Override
-	public void onProviderEnabled(String provider) {
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-	}
 }
