0383afe0ce97b1976a4d3ff4a5469ca3b1463631
Adrianus Kleemans <a.kleemans@gmail.com>
2012-04-30 19:50:06 +0200

    Improved buffering method on HTTP-Request fetching.

--- src/main/java/ch/eonum/HTTPRequest.java
+++ src/main/java/ch/eonum/HTTPRequest.java
@@ -1,10 +1,12 @@
 package ch.eonum;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.apache.http.util.ByteArrayBuffer;
 
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
@@ -77,24 +79,35 @@ public class HTTPRequest extends AsyncTask<Void, Void, String>
 	@Override
 	protected String doInBackground(Void... params)
 	{
-		Log.i(this.getClass().getName(), "Start reading answer from server with url:");
-		Log.i(this.getClass().getName(), "   " + url);
+		Timer t = new Timer();
+		Log.i(this.getClass().getName(), "Start reading answer from server at " + t.timeElapsed() + "ms");
+		Log.i(this.getClass().getName(), "   url: " + url);
+
+		final int APPROX_MAX_PAGE_SIZE = 500;
 		try
 		{
-			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-			String str;
-			while ((str = in.readLine()) != null)
+			InputStream inputStream = url.openStream();
+			BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+			ByteArrayBuffer byteArrayBuffer = new ByteArrayBuffer(APPROX_MAX_PAGE_SIZE);
+
+			byte[] buf = new byte[APPROX_MAX_PAGE_SIZE];
+			int read;
+			do
 			{
-				resultString += str;
+				read = bufferedInputStream.read(buf, 0, buf.length);
+				if (read > 0)
+					byteArrayBuffer.append(buf, 0, read);
 			}
-			in.close();
+			while (read >= 0);
+			this.resultString = new String(byteArrayBuffer.toByteArray());
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
 
-		Log.i(this.getClass().getName(), "Size of HTTP answer: " + resultString.length());
+		Log.i(this.getClass().getName(), "Size of HTTP answer: " + resultString.length() + " at " + t.timeElapsed()
+			+ "ms");
 		return resultString;
 	}
 
