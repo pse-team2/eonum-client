a6df8973f43727c292433c84ffbd025ce0e4df73
Adrianus Kleemans <a.kleemans@gmail.com>
2012-05-07 12:31:27 +0200

    Added first test, let's check if Jenkins likes it...

--- src/test/java/ch/eonum/test/HTTPRequestTest.java
+++ src/test/java/ch/eonum/test/HTTPRequestTest.java
@@ -2,11 +2,16 @@ package ch.eonum.test;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.ExecutionException;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import android.os.AsyncTask;
+import ch.eonum.HTTPRequest;
 
 /* Unit Test */
 public class HTTPRequestTest
@@ -34,12 +39,33 @@ public class HTTPRequestTest
 	@Test
 	public final void testHTTPRequest()
 	{
-		//fail("Noch nicht implementiert"); // TODO
+		HTTPRequest request = new HTTPRequest(46.90, 7.50, 46.95, 7.55);
+		String result = "";
+		AsyncTask<Void, Void, String> httpTask = request.execute();
+		try
+		{
+			result = httpTask.get();
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
+		catch (ExecutionException e)
+		{
+			e.printStackTrace();
+		}
+		
+		assertFalse(result.length() == 0);
+		assertTrue(result.contains("Lobsiger"));
+		assertTrue(result.contains("Rohrmattstrasse"));
+		assertTrue(result.contains("internisten"));
+		assertTrue(result.contains("46.92"));
 	}
 
 	@Test
 	public final void testGetResults()
 	{
-		//fail("Noch nicht implementiert"); // TODO
+		// fail("Noch nicht implementiert"); // TODO
 	}
 }
