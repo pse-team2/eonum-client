45411a0b35622f793e402c7ee537cf25947af635
Adrianus Kleemans <a.kleemans@gmail.com>
2012-05-19 13:09:53 +0200

    Added 2 test cases for JSONParser

--- src/test/java/ch/eonum/test/JSONParserTest.java
+++ src/test/java/ch/eonum/test/JSONParserTest.java
@@ -30,7 +30,7 @@ public class JSONParserTest
 	}
 
 	@Test
-	public final void testDeserializeHansMuster()
+	public final void testDeserialize()
 	{
 		String content = "{ \"status\": \"OK\",\"results\": [ { \"name\": \"Hans Muster\","
 			+ "\"address\": \"Bahnhofstrasse, 3000 Bern\", \"email\": \"\", \"tel\": \"\","
@@ -49,7 +49,7 @@ public class JSONParserTest
 	}
 
 	@Test
-	public final void testDeserializeErnstHasli()
+	public final void testDeserializeWithEmail()
 	{
 		String content = "{ \"status\": \"OK\",\"results\": [ { \"name\": \"Ernst Hasli\","
 			+ "\"address\": \"Kleines Gässli 5, 1234 Beispielsdorf\", \"email\": \"ernst_58@hotmail.com\",  \"tel\": \"\","
@@ -65,5 +65,44 @@ public class JSONParserTest
 		assertEquals("Ernst Hasli", testLocation.getName());
 		assertEquals("Kleines Gässli 5, 1234 Beispielsdorf", testLocation.getAddress());
 		assertEquals("ernst_58@hotmail.com", testLocation.getEmail());
+	}
+	
+	@Test
+	public final void testDeserializeWithTelephone()
+	{
+		String content = "{ \"status\": \"OK\",\"results\": [ { \"name\": \"Hans Mueller\","
+			+ "\"address\": \"First Street, New York\", \"email\": \"hansi@web.de\",  \"tel\": \"031 322 22 22\","
+			+ "\"types\": [ \"urologen\" ], \"location\": { \"lat\": 43.2, \"lng\": 7.56 } } ] } ";
+
+		JSONParser parser = new JSONParser();
+		MedicalLocation[] locations = parser.deserializeLocations(content);
+
+		assertNotNull(locations);
+		MedicalLocation testLocation = locations[0];
+
+		assertNotNull(testLocation);
+		assertEquals("Hans Mueller", testLocation.getName());
+		assertEquals("First Street, New York", testLocation.getAddress());
+		assertEquals("hansi@web.de", testLocation.getEmail());
+		assertEquals("031 322 22 22", testLocation.getTelephone());
+	}
+	
+	@Test
+	public final void testDeserializeMultipleCategories()
+	{
+		String content = "{ \"status\": \"OK\",\"results\": [ { \"name\": \"Bruce Wayne\","
+			+ "\"address\": \"Gotham City\", \"email\": \"batman@akauk.com\",  \"tel\": \"\","
+			+ "\"types\": [ \"akupunkturaerzte, anaesthesisten, chirurgen\" ], \"location\": { \"lat\": 40.7129, \"lng\": -74.006 } } ] } ";
+		
+		JSONParser parser = new JSONParser();
+		MedicalLocation[] locations = parser.deserializeLocations(content);
+
+		assertNotNull(locations);
+		MedicalLocation testLocation = locations[0];
+
+		assertNotNull(testLocation);
+		assertEquals("Bruce Wayne", testLocation.getName());
+		assertEquals("Gotham City", testLocation.getAddress());
+		assertEquals("batman@akauk.com", testLocation.getEmail());
 	}
 }
